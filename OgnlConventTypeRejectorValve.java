fc4813b8a1f6de76e7d820613127a92b865fecc4
Masahiro-Yamada <masa141421356@gmail.com>
2017-03-11 12:41:16 +0900

    Create OgnlConventTypeRejectorValve.java
    first edition

---
+++ OgnlConventTypeRejectorValve.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.connector.Request;
+import org.apache.catalina.connector.Response;
+import org.apache.catalina.valves.ValveBase;
+/**
+ * Valve to reject attack for Struts2 CVE-2017-5638 (S2-045)
+ */
+public class OgnlContentTypeRejectorValve extends ValveBase {
+
+    /**
+     * Default Constructor
+     */
+    public OgnlContentTypeRejectorValve() {
+        //do nothing
+    }
+
+    /**
+     * Constructor
+     * @param asyncSupported
+     */
+    public OgnlContentTypeRejectorValve(boolean asyncSupported) {
+        super(asyncSupported);
+    }
+
+    @Override
+    public String getInfo() {
+        return "OgnlContentTypeRejectorValve/1.0";
+    }
+    /* (non-Javadoc)
+     * @see org.apache.catalina.valves.ValveBase#invoke(org.apache.catalina.connector.Request, org.apache.catalina.connector.Response)
+     */
+    @Override
+    public void invoke(Request request, Response response) throws IOException,
+            ServletException {
+        final String contentType = request.getContentType();
+        // reject when content-Type contains OGNL
+        if ((contentType != null) && contentType.contains("%{")) {
+            ((HttpServletResponse)response).sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+        getNext().invoke(request, response);
+    }
+
+}
